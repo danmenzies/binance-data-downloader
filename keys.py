864768e15634c504c270d45fa7f8107be154004d
Dan Menzies <dan.menzies@aspectworld.com>
2024-05-09 20:46:18 +0100

    Changed key file, to pull from environment variables

--- keys.py
+++ keys.py
@@ -1,4 +1,13 @@
-# ADD YOUR BINANCE API_KEY AND SECRET_KEY
-binance_api_key = "_"
-binance_secret_key = "_"
+"""
+This file is used to store the API keys for the Binance API.
+"""
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables
+load_dotenv()
+
+# Binance Keys
+BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
+BINANCE_SECRET_KEY = os.getenv("BINANCE_SECRET_KEY")
 
